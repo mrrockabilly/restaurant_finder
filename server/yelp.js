fe794bd3b2731a6289c28ecbb5451dbd6c6a3ab4
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-07 16:25:14 -0700

    Added error message

--- server/yelp.js
+++ server/yelp.js
@@ -1,14 +1,21 @@
 var Yelp = require('yelp');
 
+var yelp = new Yelp({
+  consumer_key: 'BkCGfK4qtGp7jn0Ch8c2KQ',
+  consumer_secret: 'KJcinh1geGFPAtIRVUFlXStm974',
+  token: '0AKP7BszvcZ-z-gxCxxl03UNj3I1Tijz',
+  token_secret: 'et5do3f5JtQ3l6i3wYOuhdHuyCo',
+});
 
 
 module.exports.getData = function (req, res) {
-  yelp.search({ term: 'food', location: 'Irvine', limit: 20})
+  yelp.search({ term: 'food', location: 'Irvine', limit: 11110})
   .then(function (data) {
-   console.log("test");   
-   res.send(data);
+    var businesses = data["businesses"];
+    res.render('./../client/index', { businesses: businesses });
+   //res.send(data);
   })
   .catch(function (err) {
-   console.error(err);
+   res.render('./../views/error',{});
   });
 }

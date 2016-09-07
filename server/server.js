79d552305b92e8805e9ab22d2691af1695595407
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-07 14:20:09 -0700

    fixed city submit form

--- server/server.js
+++ server/server.js
@@ -2,17 +2,27 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const yelpController = require('./yelp');
 const instagramController = require('./instagram');
+const customYelpSearch = require('./customYelpSearch')
 
 //Allows CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   bodyParser();
+
+app.use(bodyParser());
+app.set('view engine', 'ejs');
 
 //The route defined as root.
+
+app.get('/search', function(req, res) {
+  res.render('./../client/query');
+});
+
+app.post('/submitQuery', customYelpSearch.getCustom);
 
 app.get('/', yelpController.getData);
 

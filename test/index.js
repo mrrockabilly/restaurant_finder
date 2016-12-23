e7617c9b0d1cb9e23ec37ee1aec82d95dcd6a045
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-12-23 13:40:38 -0800

    added test for / route

--- test/index.js
+++ test/index.js
@@ -13,30 +13,19 @@ for (var i = 0; i < routes.length; i++) {
 
 request = request(app);
 
-describe('Unit 9 Node API', function() {
-  it('should connect to api', function(done) {
+describe('Restaurant Finder should have an index page', function() {
+  it('should connect to server', function(done) {
     request
       .get('/')
       .set('Accept', 'application/json')
       .expect(200, done);
   });
 
-  it('should provide back json', function(done) {
+  it('should provide back html', function(done) {
     request
       .get('/')
       .set('Accept', 'application/json')
-      .expect('content-type', /json/)
+      .expect('content-type', /html/)
       .expect(200, done);
-  });
-
-  it('should have at least two end points', function() {
-    expect(router_counter).to.be.at.least(2);
-  });
-
-  it('should allows cross origin resource sharing (look up Access-Control-Allow-Origin)', function(done){
-    request
-      .get('/')
-      .expect('Access-Control-Allow-Origin', '*')
-      .end(done);
   });
 });

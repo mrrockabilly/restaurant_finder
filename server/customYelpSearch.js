ac858fb0ca4abd88796059f33166f3fc3ec69529
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-12-23 13:47:51 -0800

    removed unnesary code

--- server/customYelpSearch.js
+++ server/customYelpSearch.js
@@ -1,8 +1,5 @@
 var Yelp = require('yelp');
 var passwords = require('./passwords');
-// var mongoose = require('mongoose');
-// var YelpCache = require('./yelpresults');
-
 var yelp = new Yelp({
   consumer_key: passwords.yelpConsumer_key,
   consumer_secret: passwords.yelpConsumer_secret,
@@ -10,36 +7,11 @@ var yelp = new Yelp({
   token_secret: passwords.yelpToken_secret,
 });
 
-
 module.exports.getCustom = function (req, res) {
 console.log(req.body);
   yelp.search({ term: req.body.results, location: req.body.city})
   .then(function (data) {
     var businesses = data["businesses"];
-    console.log(businesses);
-
-    // var newyelpcache = new YelpCache({
-    //   name: "Test1",
-    //   text: "Test2",
-    //   phone: "Test3"
-    // });
-    // newyelpcache.save(function (err) {
-    //   if (err) {
-    //     console.log("bad");
-    //   } else {
-    //     console.log("good");
-    //   }
-    // })
-
-
-    // YelpCache.find({}, function(err, data){
-    //     if (err){
-    //         console.log("the error", data)
-    //     } else {
-    //         console.log(err)
-    //     }
-    // })
-
     res.render('./../views/index', { businesses: businesses });
   })
   .catch(function (err) {
